787abccf4042b0e8dc0d30c6733cc1c0b44d24fd
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2023-01-27 22:04:58 -0300

    Button styles and classNames

--- src/calculator/Calculator.jsx
+++ src/calculator/Calculator.jsx
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import './Calculator.css';
 
 import Button from '../components/Button';
-import { Display } from '../components/Display';
+import Display  from '../components/Display';
+
 
 export default class Calculator extends Component {
+
     render() {
         return(
             <div className='calculator'>
